48e9c82a303a6ca91181c05012591e6657fa20c0
Julius Vitkauskas <julius.vitkauskas@trafi.com>
2020-11-19 23:41:10 +0200

    Initial commit

---
+++ EldesReader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using HidLibrary;
+
+namespace EldesReader
+{
+    class Program
+    {
+        private static readonly int UsbVendorId = 0xc201;
+        private static readonly int UsbProductId = 0x1318;
+
+        static void Main(string[] args)
+        {
+            var device = HidDevices.Enumerate(UsbVendorId, UsbProductId).FirstOrDefault();
+            if (device is null)
+            {
+                Console.WriteLine("Device not connected");
+                return;
+            }
+
+            var usbReader = new UsbReader(device);
+
+            var clockResponse = usbReader.MakeRequest("clock", "clck");
+            Console.WriteLine($"Clock response: {clockResponse}");
+            
+            var uptimeResponse = usbReader.MakeRequest("uptime", "UPT");
+            Console.WriteLine($"Uptime response: {uptimeResponse}");
+            
+            var zonesResponse = usbReader.MakeRequest("zstatus");
+            Console.WriteLine($"Zones response: {zonesResponse}");
+
+            var zoneStatusHexStr = zonesResponse[14..16]; // eg. zstatus:0000002E000000000000000000000000000000 => 2E
+            var zoneStatusInt = int.Parse(zoneStatusHexStr, NumberStyles.HexNumber);
+            var zones = new BitArray(new[] {zoneStatusInt});
+
+            int i = 0;
+            foreach (bool zone in zones)
+            {
+                Console.WriteLine($"Zone {++i}: {(zone ? "Open" : "Closed")}");
+            }
+        }
+    }
+}
---
+++ EldesReader/UsbReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using HidLibrary;
+
+namespace EldesReader
+{
+    public class UsbReader
+    {
+        private readonly HidDevice _device;
+        private readonly int _packetLength;
+
+        public UsbReader(HidDevice device)
+        {
+            _device = device;
+            _packetLength = device.Capabilities.OutputReportByteLength - 1;
+        }
+        
+        public string MakeRequest(string command, string? responseText = null)
+        {
+            var requestData = new byte[_packetLength];
+
+            var request = Encoding.ASCII.GetBytes($"{(char) (command.Length+1)}{command}\r");
+            Array.Copy(request, 0, requestData, 1, request.Length);
+
+            _device.Write(requestData);
+
+            while (true) // TODO: timeout?
+            {
+                var responseData = _device.Read();
+                
+                var responseLength = responseData.Data[1];
+
+                if (responseLength == 0)
+                {
+                    continue;
+                }
+                
+                var response = Encoding.ASCII.GetString(responseData.Data[2..][..responseLength]);
+
+                if (response.Contains(responseText ?? command))
+                {
+                    return response;
+                }
+            }
+        }
+    }
+}
